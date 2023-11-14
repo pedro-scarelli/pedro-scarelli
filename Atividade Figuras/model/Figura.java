84be1f9dcc1299c25239a80033a3d94a8bb6adef
Pedro Virissimo Scarelli <144300492+pvscarelli@users.noreply.github.com>
2023-11-14 14:32:19 -0300

    Delete Atividade Figuras directory

--- Atividade Figuras/model/Figura.java
+++
@@ -1,6 +0,0 @@
-package L10.Q1.model;
-
-public abstract class Figura {
-
-    public abstract double calcularArea();
-}
--- Atividade Figuras/model/Quadrado.java
+++
@@ -1,8 +0,0 @@
-package L10.Q1.model;
-
-public class Quadrado extends Retangulo {   
-    
-    public Quadrado (int medida){
-        super(medida, medida);// chamo o construtor da superclasse retangulo, pois quadrado é só um tipo de retangulo com os lados iguais
-    }   
-}
